42fcf7c9c619828a7ce3da7be36627b2106ee2a1
Elis-code <Ewcurran@cps.edu>
2025-11-04 10:01:44 -0600

    finished

--- a4.py
+++ a4.py
@@ -3,16 +3,30 @@
 
 
 class TTTBoard:
-    """A tic tac toe board
+    def __init__(self):
+        self.board = ['*'] * 9
 
-    Attributes:
-        board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
-            represent moves by player 'O' and '*'s are spots no one has yet played on
-    """
+    def __str__(self):
+        """return a string representation of the board """
+        return f"{self.board[0]} {self.board[1]} {self.board[2]}\n{self.board[3]} {self.board[4]} {self.board[5]}\n{self.board[6]} {self.board[7]} {self.board[8]}"
 
-    pass
-
-
+    def make_move(self, player, pos):
+        if 0 <= pos <= 8 and self.board[pos] == '*':
+            self.board[pos] = player
+            return True
+        return False
+    def has_won(self, player):
+        winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        for combo in winning_combos:
+            if self.board[combo[0]] == player and self.board[combo[1]] == player and self.board[combo[2]] == player:
+                return True
+        return False
+    def game_over(self):
+        """Check if the game is over (someone has won or the board is full)"""
+        return self.has_won("X") or self.has_won("O") or '*' not in self.board
+    def clear(self):
+        self.board = ['*'] * 9
+    
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
--- a4_writeup.md
+++ a4_writeup.md
@@ -5,7 +5,8 @@ In assignment 4 we created a basic tic tac toe game so that we could learn objec
 ## Reflection Questions
 
 1. What was the most difficult part to tic-tac-toe?
-
+at first I wanted to make a 2d aray to make the tic tac toe board but it was to hard for me to understand at this point. it resulted in me using a normal list. 
 2. Explain how you would add a computer player to the game.
-
+I would have a computer be assined a letter, once it's assined a letter it would wait for a move from the player then make a move depending on where the player played. it would know what moves lead to a win and what moves lead to one away from a win. 
 3. If you add a computer player, explain (doesn't have to be super technical) how you might get the computer player to play the best move every time. *Note - I am not grading this for a correct answer, I just want to know your thoughts on how you might accomplish it.
+I would have it know all the possible moves that could have it win and have a function scan to see if the player was one away from any of those so that it could stop them from happaning. after enough games i'm sure it could win one. 
